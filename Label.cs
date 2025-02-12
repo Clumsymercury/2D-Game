3793caaa70d7b2914040a123eb86ab5e057a90d1
Oliver <pl63fun@eduvaud.ch>
2025-02-12 16:15:43 +0100

    test retour de commit

--- Label.cs
+++ Label.cs
@@ -6,7 +6,7 @@ public partial class Label : Godot.Label
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Text = "Hello World et oliver";
+		Text = "Hello World et oliver et alex";
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
