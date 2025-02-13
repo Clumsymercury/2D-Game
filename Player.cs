c6c2d7bc88715d776235d5c5759372340560ecef
Alexandre <Alexandre.ganshof@eduvaud.ch>
2025-02-14 00:52:56 +0100

    debut de player mouvement and small map

---
+++ Player.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using Godot;
+
+public partial class Player : CharacterBody2D
+{
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+	private Vector2 Direction;
+	private float Speed = 200;
+
+    public override void _Input(InputEvent @event)
+    {
+        base._Input(@event);
+		Direction = Input.GetVector("MoveLeft", "MoveRight", "MoveUp", "MoveDown");
+    }
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+		Velocity = Direction * Speed;
+
+		MoveAndSlide();
+    }
+}
