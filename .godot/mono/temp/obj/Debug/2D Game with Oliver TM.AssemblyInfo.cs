35b3b9be06dc5e6dfaa6d904ab2b0acdf4f2d925
Alexandre <Alexandre.ganshof@eduvaud.ch>
2025-10-02 01:16:23 +0200

    enlever le bush

--- .godot/mono/temp/obj/Debug/2D Game with Oliver TM.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/2D Game with Oliver TM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("2D Game with Oliver TM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70f20fca10c8ff147931bea38dffda6213d0dd83")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5664c2355060bea7e1491f92b322849bd7292c42")]
 [assembly: System.Reflection.AssemblyProductAttribute("2D Game with Oliver TM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("2D Game with Oliver TM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
